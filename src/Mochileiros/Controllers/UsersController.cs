99e098b86b0cfc4cf45c76bb314720badeafb722
MarianaRodrigues97 <Mariana.proativo@gmail.com>
2024-04-30 22:42:16 -0300

    Adicionando cotrolers usuários

---
+++ src/Mochileiros/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mochileiros.Models;
+
+
+namespace Mochileiros.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+      
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
