0f0d50cea9fdd35c18e13e26014062406bd944db
IgorIFA <141268716+IgorIFA@users.noreply.github.com>
2024-06-06 19:32:18 -0300

    Update UsersController.cs

--- src/Mochileiros/Controllers/UsersController.cs
+++ src/Mochileiros/Controllers/UsersController.cs
@@ -9,42 +9,127 @@ using Mochileiros.Data;
 
 namespace Mochileiros.Controllers
 {
-    public class UsersController : Controller
+        public class UsuariosController : Controller
     {
-        private readonly ILogger<UsersController> _logger;
-        private readonly MochileirosContext _context;
+        private readonly AppDbContext _context;
 
-        public UsersController(ILogger<UsersController> logger)
+        public UsuariosController(AppDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
-      
-        public async Task<IActionResult> Viagem(int? id)
-        {
-            if(id == null)
-                return NotFound();
-            var usuario = await _context.User.FindAsync(id);
 
-            if (usuario == null)
-                return NotFound();
-            var viagens = await _context.Travel
-                .Where(t => t.UserId == id)
-                .OrderByDescending(t => t.StartDate)
-                .ToListAsync();
-            
-            ViewBag.Travel = viagens;
-      
+        // GET: Usuarios
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Usuarios.ToListAsync());
+        }
+
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
             return View();
         }
+
+        [AllowAnonymous]
         public IActionResult Login()
         {
             return View();
         }
- public async Task<IActionResult> Newuser()
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(Usuario usuario)
         {
-            
-      
+            var dados = await _context.Usuarios
+                .FindAsync(usuario.Id);
+
+            if(dados == null)
+            {
+                ViewBag.Message = "Usuário e/ou senha invalidos!";
+                return View();
+            }
+
+            bool senhaOk = BCrypt.Net.BCrypt.Verify(usuario.Senha, dados.Senha);
+
+            if (senhaOk)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, dados.Nome),
+                    new Claim(ClaimTypes.NameIdentifier, dados.Id.ToString()),
+                    new Claim(ClaimTypes.Role, dados.Perfil.ToString())
+                };
+
+                var usuarioIdentity = new ClaimsIdentity(claims, "login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(usuarioIdentity);
+
+                var props = new AuthenticationProperties
+                {
+                    AllowRefresh = true,
+                    ExpiresUtc = DateTime.UtcNow.ToLocalTime().AddHours(8),
+                    IsPersistent = true,
+                };
+
+                await HttpContext.SignInAsync(principal, props);
+
+                return Redirect("/");
+            }
+            else
+            {
+                ViewBag.Message = "Usuário e/ou senha invalidos!";
+            }
+                
             return View();
         }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction("Login", "Usuarios");
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Usuarios == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Senha,Perfil")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
     }
 }
